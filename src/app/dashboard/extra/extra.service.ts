ee7136d6206fa055d0ed03804e7c1aa89c5368d7
crayolito <78750684+crayolito@users.noreply.github.com>
2024-12-20 11:44:03 -0400

    update

--- src/app/dashboard/extra/extra.service.ts
+++ src/app/dashboard/extra/extra.service.ts
@@ -36,7 +36,8 @@ interface ComunicadoCreateRequest {
 })
 export class ExtraService {
   private http = inject(HttpClient);
-  private apiUrl = 'http://localhost:3000/api';
+  // READ : MODIFICAR AQUI PARA EL EXAMEN
+  private apiUrl = 'https://parkingsw1-188f4effa11e.herokuapp.com/api';
 
   // Servicios CRUD
   createServicio(
--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -26,7 +26,7 @@ export interface EmpresaInfo {
 })
 export class HomeService {
   public http = inject(HttpClient);
-  private apiUrl = 'http://localhost:3000/api';
+  private apiUrl = 'https://parkingsw1-188f4effa11e.herokuapp.com/api';
   public empresaInfo = signal<EmpresaInfo | null>(null);
 
   registrarEmpresaParking(
